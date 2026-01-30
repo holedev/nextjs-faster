374cad98b3bb83e8b15ce9bddf1d1c1b8d948cd5
holedev <phanleho002@gmail.com>
2026-01-30 11:50:59 +0700

    chore(ultracite): upgrade ver ultracite

--- components/custom/Header/ModeToggle.client.tsx
+++ components/custom/Header/ModeToggle.client.tsx
@@ -19,7 +19,7 @@ const ModeToggle = () => {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button size='icon' variant='outline'>
-          <SunIcon className='dark:-rotate-90 h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:scale-0' />
+          <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
           <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
